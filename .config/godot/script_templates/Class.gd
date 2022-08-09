81916065a46d6376b1f7ff7ad94e8955f434bf5a
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-10 01:14:25 +0200

    godot settings and themes

---
+++ .config/godot/script_templates/Class.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name Template#, 'res://editor_icons/icon.svg'
+func get_class() -> String: return 'Template'
+
+signal temp_signal(temp)
+
+export (int) var member: int
+
+var _member_2
+
+func _init() -> void:
+  pass
+
+onready var member_3
+
+func _ready() -> void:
+  pass
+
+func _process(delta: float) -> void:
+  pass
+
+func _physics_process(delta: float) -> void:
+  pass
